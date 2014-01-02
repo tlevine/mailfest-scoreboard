6a5dfa1c71bca8bc62a63f6ccd62daab9a5e3d67
Thomas Levine <occurrence@thomaslevine.com>
2014-01-02 21:22:33 +0000

    length statistics

--- stats.py
+++ stats.py
@@ -4,4 +4,20 @@ email.message.Message and returns
 a string, boolean, float or int.
 Or None.
 '''
+from io import StringIO as _StringIO
+from tokenize import generate_tokens as _generate_tokens
 
+def _ilen(iterable) -> int:
+    'Calculate the length iteratively.'
+    return sum(1 for _ in iterable)
+
+def body_characters(email) -> int:
+    'Number of characters in the body'
+    return len(email.get_payload())
+
+def body_words(email) -> int:
+    'Number of words in the body'
+    return _ilen(_generate_tokens(_StringIO(email.get_payload()).readline))
+
+def body_lines(email) -> int:
+    return _ilen(1 for _ in email.get_payload() if _ == '\n')
