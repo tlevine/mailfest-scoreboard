6b5e9608b2961463f1fa842a30a636769f54894e
Thomas Levine <occurrence@thomaslevine.com>
2014-01-02 21:50:03 +0000

    attachments

--- stats.py
+++ stats.py
@@ -4,25 +4,36 @@ email.message.Message and returns
 a string, boolean, float or int.
 Or None.
 '''
+import builtins as _builtins
 import re as _re
 from io import StringIO as _StringIO
 from tokenize import generate_tokens as _generate_tokens
+
+def _body(email) -> str:
+    return _payload(email)[0].get_payload()
+
+def _payload(email) -> str:
+    p = email.get_payload()
+    if isinstance(p, _builtins.str):
+        return [email]
+    else:
+        return p
 
 def _ilen(iterable) -> int:
     'Calculate the length iteratively.'
     return sum(1 for _ in iterable)
 
 def body_characters(email) -> int:
-    'Number of characters in the body'
-    return len(email.get_payload())
+    'Number of characters in the body text'
+    return len(_body(email))
 
 def body_words(email) -> int:
-    'Number of words in the body'
-    return _ilen(_generate_tokens(_StringIO(email.get_payload()).readline))
+    'Number of words in the body text'
+    return _ilen(_generate_tokens(_StringIO(_body(email)).readline))
 
 def body_lines(email) -> int:
     'Number of lines in the body'
-    return _ilen(1 for _ in email.get_payload() if _ == '\n')
+    return _ilen(filter(lambda x: x == '\n', _body(email)))
 
 '''
 def to_addresses(email) -> int:
@@ -54,3 +65,10 @@ def gmail(email) -> bool:
     (whether the earliest/lowest "Received:" header includes "google.com ")
     '''
     return 'google.com ' in email.get_all('received')[-1]
+
+def attachments(email) -> int:
+    'Number of attachments'
+    msgs = _payload(email)
+    def is_attachment(msg) -> bool:
+        return None == _re.match(r'^text/.*$', msg.get_content_type())
+    return _ilen(filter(is_attachment, msgs))
