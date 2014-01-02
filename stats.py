97a780af71a116888dad06b5870a95f189b61858
Thomas Levine <occurrence@thomaslevine.com>
2014-01-02 20:50:23 +0000

    informal type annotation

--- stats.py
+++ stats.py
@@ -1,2 +1,9 @@
-def from_address(email:str) -> str:
+'''
+Each of these functions takes an
+email.message.Message and returns
+a string, boolean, float or int.
+Or None.
+'''
+
+def from_address(email) -> str:
     return 'foo@bar.baz'
