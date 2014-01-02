013888afcca9047df7a2068ab7aa520520b0f571
Thomas Levine <occurrence@thomaslevine.com>
2014-01-02 21:31:35 +0000

    more stats

--- stats.py
+++ stats.py
@@ -4,6 +4,7 @@ email.message.Message and returns
 a string, boolean, float or int.
 Or None.
 '''
+import re as _re
 from io import StringIO as _StringIO
 from tokenize import generate_tokens as _generate_tokens
 
@@ -20,4 +21,29 @@ def body_words(email) -> int:
     return _ilen(_generate_tokens(_StringIO(email.get_payload()).readline))
 
 def body_lines(email) -> int:
+    'Number of lines in the body'
     return _ilen(1 for _ in email.get_payload() if _ == '\n')
+
+'''
+def to_addresses(email) -> int:
+    'Number of addresses in the "To:" field'
+    return len(email.get_all('to'))
+
+def cc_addresses(email) -> int:
+    'Number of addresses in the "CC:" field'
+    return len(email.get_all('cc'))
+'''
+
+def hops(email) -> int:
+    'Number of "Received" headers in the email'
+    return len(email.get_all('received'))
+
+def subject_re(email) -> bool:
+    'Whether the subject indicates a reply (starts with /^re:/)'
+    subject = email.get('subject')
+    _re.match(r'^re:.*$', subject, flags = _re.IGNORECASE) != None
+
+def subject_fwd(email) -> bool:
+    'Whether the subject indicates a forward (starts with /^fwd:/)'
+    subject = email.get('subject')
+    _re.match(r'^fwd:.*$', subject, flags = _re.IGNORECASE) != None
