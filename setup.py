c78bdece66f6046f52d4fd7fbb912d356f0b36e9
Thomas Levine <occurrence@thomaslevine.com>
2014-01-02 19:03:20 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='mailfest-scoreboard',
+      version='0.0',
+      description='Score mailfest participants',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      url='https://github.com/tlevine/mailfest-scoreboard',
+      scripts=['scoreboard'],
+      test_suite='tests',
+     )
