eb654cc8ca8c7b80e760eefabb83bc9d926bca2e
Thomas Levine <occurrence@thomaslevine.com>
2014-01-03 16:31:06 +0000

    test payload

--- test_features.py
+++ test_features.py
@@ -1,11 +1,14 @@
 import os
 from email import message_from_file
+from email.message import Message
+import builtins
 
 import nose.tools as n
 
 import headers, stats
 
 email = message_from_file(open(os.path.join('fixtures','attachments')))
+
 def test_headers():
     observed = headers.headers(email)
     expected = {
@@ -15,3 +18,15 @@ def test_headers():
         'content-type': 'multipart/mixed; boundary="AqsLC8rIMeq19msA"'
     }
     n.assert_dict_equal(observed, expected)
+
+def test_ilen():
+    n.assert_equal(stats._ilen(range(100)), len(range(100)))
+
+def test_body():
+    n.assert_is_instance(stats._body(email), str)
+
+def test_body():
+    payload = stats._payload(email)
+    n.assert_not_is_instance(payload, builtins.str)
+    for bomb in payload:
+        n.assert_is_instance(bomb, Message)
