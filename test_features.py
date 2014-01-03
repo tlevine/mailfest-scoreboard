c452ef7d968070014825f9ae160c7e57175565f5
Thomas Levine <occurrence@thomaslevine.com>
2014-01-03 16:26:03 +0000

    test headers

--- test_features.py
+++ test_features.py
@@ -1,4 +1,17 @@
 import os
 from email import message_from_file
 
+import nose.tools as n
+
+import headers, stats
+
 email = message_from_file(open(os.path.join('fixtures','attachments')))
+def test_headers():
+    observed = headers.headers(email)
+    expected = {
+        'date': 'Thu, 2 Jan 2014 21:37:44 +0000',
+        'from': 'Thomas Levine <_@thomaslevine.com>',
+        'user-agent': 'Mutt/1.5.22 (2013-10-16)',
+        'content-type': 'multipart/mixed; boundary="AqsLC8rIMeq19msA"'
+    }
+    n.assert_dict_equal(observed, expected)
